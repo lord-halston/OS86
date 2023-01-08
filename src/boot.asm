91d58d79e65d2dce9b05aa4f11d47059e2555cc3
lord-halston <122018978+lord-halston@users.noreply.github.com>
2023-01-08 00:16:46 -0800

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -8,7 +8,7 @@ int 0x10						; BIOS Video Interrupt
 
 mov ah, 0x02					; AH = 0x02/INT 0x10 - Set Cursor Position
 mov dh, 1						; Set row
-mov dl, 35						; Set column
+mov dl, 37						; Set column
 int 0x10						; BIOS Video Interrupt
 
 mov ah, 0x0B					; AH = 0x0B/INT 0x10 - Set Color Palette
@@ -16,16 +16,29 @@ mov bh, 0x00					; Set background color
 mov bl, 0x04					; Red
 int 0x10						; BIOS Video Interrupt
 
-mov ah, 0x0E					; AH = 0x0E/INT 0x10 - BIOS Teletype Output
 mov bx, osName					; Move memory address at osName into BX register
-
 call puts						; Print string stored in BX
+
+mov bx, hexTest
+call puts
+
+mov ah, 0x02					; AH = 0x02/INT 0x10 - Set Cursor Position
+mov dh, 3						; Set row
+mov dl, 1						; Set column
+int 0x10						; BIOS Video Interrupt
+
+int 0x12						; BIOS Memory Size Determination
+
+mov dx, ax
+call puthex
 
 jmp $							; Jump to current address (loop indefinitely)
 
 include '../include/puts.asm'
+include '../include/puthex.asm'
 
-osName:	db 'OS86', 0			; Null-terminated string to print
+osName:	db 'OS86', 0xA, 0xD, 0			
+hexTest: db 'Memory Size: ', 0
 
 times 510-($-$$) db 0			; Pad out 0s until 510th byte
 dw 0xAA55						; Magic number for BIOS boot
